25fa7eddd4a8d027a5f0a90c105f7eada3aff00c
Martin <3014683970101@ingenieria.usac.edu.gt>
2023-08-01 19:52:02 -0600

    Commit blade

--- resources/views/cursos/create.blade.php
+++ resources/views/cursos/create.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Esta es la página para crear un curso</h1>
-</body>
-</html>
+@extends('layouts.template')
+
+@section('title', 'Cursos create')
+    
+@section('content')
+     <h1>En esta pagina podras crear cursos</h1>
+@endsection
--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Esta es la página principal de cursos</h1>
-</body>
-</html>
+@extends('layouts.template')
+
+@section('title', 'Cursos')
+    
+@section('content')
+     <h1>Esta es la página principal de cursos</h1>
+@endsection
